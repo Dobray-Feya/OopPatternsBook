198ef2048dec3638b101ca502e78f546307d26e0
Polina Arkashova <arkashova_p@mail.ru>
2023-09-24 02:03:05 +0700

    Add project files.

---
+++ DucksApp_Strategy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DucksApp_Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
